3cb6158d337416557f491231d14b79d593588244
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2019-08-16 23:35:28 -0500

    Fix indentation

--- source/main.c
+++ source/main.c
@@ -40,81 +40,81 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //---------------------------------------------------------------------------------
 int main(int argc, char* argv[]) {
 //---------------------------------------------------------------------------------
-	// Init libs
-	gfxInitDefault();
-	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
-	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
-	C2D_Prepare();
-	consoleInit(GFX_BOTTOM, NULL);
+  // Init libs
+  gfxInitDefault();
+  C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+  C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+  C2D_Prepare();
+  consoleInit(GFX_BOTTOM, NULL);
 
-	// Create screens
-	C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
-
-
-	u32 clrRec = C2D_Color32(0xAB, 0x3F, 0x3F, 0xFF);
-	u32 clrClear = C2D_Color32(0x23, 0x23, 0x23, 0x68);
-
-	Point testBallPosition;
-	testBallPosition.X = SCREEN_WIDTH / 2;
-	testBallPosition.Y = SCREEN_HEIGHT / 2;
-	Point testBallVelocity;
-	testBallVelocity.X = 1;
-	testBallVelocity.Y = 1;
-	BreakerBall testBall;
-	testBall.Position = &testBallPosition;
-	testBall.Velocity = &testBallVelocity;
-	testBall.Saveable = 1;
-	LevelRuntimeData testData;
-	testData.Breakers[0] = &testBall;
-	testData.BreakerCount = 1;
-	testData.PadPosition = (SCREEN_WIDTH / 2) - (PAD_LENGTH / 2);
+  // Create screens
+  C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
 
 
-	// Main loop
-	while (aptMainLoop())
-	{
-		hidScanInput();
-		touchPosition touch;
-		hidTouchRead(&touch);
+  u32 clrRec = C2D_Color32(0xAB, 0x3F, 0x3F, 0xFF);
+  u32 clrClear = C2D_Color32(0x23, 0x23, 0x23, 0x68);
 
-		if (touch.px >= TOUCH_PADDING & touch.px <= TOUCH_WIDTH - TOUCH_PADDING) {
-			short interpolation_id = touch.px - TOUCH_PADDING;
-			long interpolation = interpolation_id * SCREEN_WIDTH / (TOUCH_WIDTH - TOUCH_PADDING * 2);
-			testData.TargetPadPosition = interpolation - (PAD_LENGTH / 2);
-		}
+  Point testBallPosition;
+  testBallPosition.X = SCREEN_WIDTH / 2;
+  testBallPosition.Y = SCREEN_HEIGHT / 2;
+  Point testBallVelocity;
+  testBallVelocity.X = 1;
+  testBallVelocity.Y = 1;
+  BreakerBall testBall;
+  testBall.Position = &testBallPosition;
+  testBall.Velocity = &testBallVelocity;
+  testBall.Saveable = 1;
+  LevelRuntimeData testData;
+  testData.Breakers[0] = &testBall;
+  testData.BreakerCount = 1;
+  testData.PadPosition = (SCREEN_WIDTH / 2) - (PAD_LENGTH / 2);
 
-		// Respond to user input
-		u32 kDown = hidKeysDown();
-		if (kDown & KEY_START)
-			break; // break in order to return to hbmenu
-		printf("\x1b[1;1HAttempt at simple 3ds pong game.");
-		printf("\x1b[2;1HCPU:     %6.2f%%\x1b[K", C3D_GetProcessingTime()*6.0f);
-		printf("\x1b[3;1HGPU:     %6.2f%%\x1b[K", C3D_GetDrawingTime()*6.0f);
-		printf("\x1b[4;1HCmdBuf:  %6.2f%%\x1b[K", C3D_GetCmdBufUsage()*100.0f);
-		/*
-		printf("\x1b[5;1H(*testData.Breakers[0]->Position).X:  %d%%\x1b[K", (*testData.Breakers[0]->Position).X);
-		printf("\x1b[6;1H(*testData.Breakers[0]->Position).Y:  %d%%\x1b[K", (*testData.Breakers[0]->Position).Y);
-		printf("\x1b[7;1H(*testData.Breakers[0]->Velocity).X:  %d%%\x1b[K", (*testData.Breakers[0]->Velocity).X);
-		printf("\x1b[8;1H(*testData.Breakers[0]->Velocity).Y:  %d%%\x1b[K", (*testData.Breakers[0]->Velocity).Y);
-		printf("\x1b[9;1HTouch:  %03d; %03d", touch.px, touch.py);*/
 
-		if (Level_TickLevel(&testData)) {
-			break;
-		}
+  // Main loop
+  while (aptMainLoop())
+  {
+    hidScanInput();
+    touchPosition touch;
+    hidTouchRead(&touch);
 
-				// Render the scene
-		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-		C2D_TargetClear(top, clrClear);
-		C2D_SceneBegin(top);
+    if (touch.px >= TOUCH_PADDING & touch.px <= TOUCH_WIDTH - TOUCH_PADDING) {
+      short interpolation_id = touch.px - TOUCH_PADDING;
+      long interpolation = interpolation_id * SCREEN_WIDTH / (TOUCH_WIDTH - TOUCH_PADDING * 2);
+      testData.TargetPadPosition = interpolation - (PAD_LENGTH / 2);
+    }
 
-		Level_DrawLevel(&testData, clrRec, clrRec);
+    // Respond to user input
+    u32 kDown = hidKeysDown();
+    if (kDown & KEY_START)
+      break; // break in order to return to hbmenu
+    printf("\x1b[1;1HAttempt at simple 3ds pong game.");
+    printf("\x1b[2;1HCPU:     %6.2f%%\x1b[K", C3D_GetProcessingTime()*6.0f);
+    printf("\x1b[3;1HGPU:     %6.2f%%\x1b[K", C3D_GetDrawingTime()*6.0f);
+    printf("\x1b[4;1HCmdBuf:  %6.2f%%\x1b[K", C3D_GetCmdBufUsage()*100.0f);
+    /*
+    printf("\x1b[5;1H(*testData.Breakers[0]->Position).X:  %d%%\x1b[K", (*testData.Breakers[0]->Position).X);
+    printf("\x1b[6;1H(*testData.Breakers[0]->Position).Y:  %d%%\x1b[K", (*testData.Breakers[0]->Position).Y);
+    printf("\x1b[7;1H(*testData.Breakers[0]->Velocity).X:  %d%%\x1b[K", (*testData.Breakers[0]->Velocity).X);
+    printf("\x1b[8;1H(*testData.Breakers[0]->Velocity).Y:  %d%%\x1b[K", (*testData.Breakers[0]->Velocity).Y);
+    printf("\x1b[9;1HTouch:  %03d; %03d", touch.px, touch.py);*/
 
-		C3D_FrameEnd(0);
-	}
+    if (Level_TickLevel(&testData)) {
+      break;
+    }
 
-	// Deinit libs
-	C2D_Fini();
-	C3D_Fini();
-	gfxExit();
-	return 0;
+        // Render the scene
+    C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+    C2D_TargetClear(top, clrClear);
+    C2D_SceneBegin(top);
+
+    Level_DrawLevel(&testData, clrRec, clrRec);
+
+    C3D_FrameEnd(0);
+  }
+
+  // Deinit libs
+  C2D_Fini();
+  C3D_Fini();
+  gfxExit();
+  return 0;
 }
